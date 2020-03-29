0dad8713a20f2a63a05e2e11b466975a8b828ce0
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 02:38:50 +0000

    first commit

---
+++ lib/breaking_bad_project1.rb
@@ -0,0 +1,6 @@
+require "breaking_bad_project1/version"
+
+module BreakingBadProject1
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/breaking_bad_project1/version.rb
@@ -0,0 +1,3 @@
+module BreakingBadProject1
+  VERSION = "0.1.0"
+end
